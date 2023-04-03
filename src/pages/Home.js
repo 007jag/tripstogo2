670b84c246464ca2211036f538787947521b552a
007jag <atifasim@Atifs-MacBook-Air.local>
2023-04-03 16:07:21 +0500

    header implemented in home page

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../components/Header";
+import "../assets/css/home.css";
+export const Home = () => {
+  return (
+    <div className="Home">
+      <Header />
+    </div>
+  );
+};
