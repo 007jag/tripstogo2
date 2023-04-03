7e14d0a94b7070e09397dff6798c9de30930007e
007jag <atifasim@Atifs-MacBook-Air.local>
2023-04-03 16:10:01 +0500

    Testing the infobox with information

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Header } from "../components/Header";
 import "../assets/css/home.css";
+import { InfoBox } from "../components/Homepage/InfoBox";
 export const Home = () => {
   return (
     <div className="Home">
       <Header />
-      <div className="boxes-wrapper common_width"></div>
+      <div className="boxes-wrapper common_width">
+        <InfoBox
+          title="Cappadocia, Turkey"
+          img="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Cappadocia_balloon_trip%2C_Ortahisar_Castle_%2811893715185%29.jpg/516px-Cappadocia_balloon_trip%2C_Ortahisar_Castle_%2811893715185%29.jpg"
+          description={`Cappadocia is a historical region in Central Anatolia, Turkey. It is largely in the provinces of Nevşehir, Kayseri, Aksaray, Kırşehir, Sivas and Niğde.`}
+        />
+      </div>
     </div>
   );
 };
